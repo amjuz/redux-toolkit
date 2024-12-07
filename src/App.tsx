44674eec196982ee27274818b98d7df1912b5f25
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-12-07 07:35:17 +0530

    Added Todo and Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,85 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./app/store";
+import {
+  addTodo,
+  removeTodo,
+  TodoItem,
+} from "./features/counter/todoSlice";
+import React, { useState } from "react";
 
 function App() {
+  // const count = useSelector((state: RootState) => state.counterSlice.value);
+  // const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState(false);
+
+  const todos = useSelector((state: RootState) => state.todoSlice);
+  const dispatch = useDispatch();
+
+  function handleAddTodo() {
+    if (!title) {
+      alert("please add title");
+      return;
+    }
+    const newTodo: Omit<TodoItem, "id"> = {
+      status,
+      title,
+    };
+    dispatch(addTodo(newTodo));
+  }
+
+  function handleRemove(id: string) {
+    dispatch(removeTodo(id));
+  }
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-950 via-blue-700 to-blue-950 ">
-      <div className="border">
+    <div className="flex justify-center text-white items-center h-screen bg-gradient-to-r from-blue-950 via-blue-700 to-blue-950 ">
+      <div className="border p-5">
         <h2 className="text-2xl font-bold text-white">Todo</h2>
+        {/* <div className="">
+          <button onClick={() => dispatch(increment())}>+</button>
+          <span>{count}</span>
+          <button onClick={() => dispatch(decrement())}>-</button>
+        </div> */}
+        <div className="flex gap-3">
+          <input
+            type="text"
+            placeholder="title"
+            onChange={(e) => setTitle(e.target.value)}
+            className="text-black"
+          />
+          <button
+            className="px-10 py-3 border-white border-4 rounded-xl"
+            onClick={handleAddTodo}
+          >
+            ADD TODO
+          </button>
+          {/* <button className="px-10 py-3 border-white border-4 rounded-xl ">
+            REMOVE TODO
+          </button> */}
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            onClick={() => setStatus(!status)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          {todos.map((todo) => (
+            <div className="border-2 p-2 rounded-xl ">
+              title:
+              <p className="flex justify-between">
+                {todo.title}
+                <button
+                  className="border-2 px-2 rounded-lg"
+                  onClick={() => handleRemove(todo.id)}
+                >
+                  REMOVE
+                </button>
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,13 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import counterSlice from "../features/counter/counterSlice";
+import todoSlice from "../features/counter/todoSlice";
 
 export const store = configureStore({
-    reducer: counterSlice
+    reducer: { counterSlice, todoSlice }
 })
 
 export type AppStore = typeof store
-export type RootState = ReturnType<AppStore['dispatch']>
+export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
 ThunkReturnType,
